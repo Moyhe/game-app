2c809738830f1f0210069af462fa143aeec95514
Mohye <dragonsinmeliodas777@gmail.com>
2024-05-25 16:11:16 +0300

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,10 @@
+
+
+const getCroppedImageUlr = (url: string) => {
+    
+    const media = 'media/';
+    const index = url.indexOf(media) + media.length;
+    return url.slice(0, index) + 'crop/600/400/' + url.slice(index);  
+}
+
+export default getCroppedImageUlr;
