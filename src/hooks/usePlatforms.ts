84465caac27e5526afe617fb0edbb96c8dc132b4
Mohye <dragonsinmeliodas777@gmail.com>
2024-06-08 21:03:34 +0300

    Refactor: Create lookup hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -15,7 +15,7 @@ const usePlatforms = () => useQuery({
   queryKey: ['platforms'],
   queryFn: apiClient.getAll,
   staleTime: 24 * 60 * 60 * 1000,
-  initialData: { count: 0, results: platforms }
+  initialData: platforms
 });
 
 export default usePlatforms;
