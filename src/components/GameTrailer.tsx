0d5d232cbc8aa0201e293d0261a8f1c9e51810fe
Mohye <dragonsinmeliodas777@gmail.com>
2024-06-10 18:17:52 +0300

    Fix the navbar

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -6,15 +6,15 @@ interface Props {
 
 const GameTrailer = ({ gameId }: Props) => {
   const { data, isLoading, error } = useTrailers(gameId);
-  console.log(data);
-
-  const first = data?.results[0];
 
   if (isLoading) return null;
 
-  if (error || !first) throw error;
+  if (error) throw error;
 
-  return <video src={first.data[480]} poster={first.preview} controls />;
+  const first = data?.results[0];
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
 };
 
 export default GameTrailer;
